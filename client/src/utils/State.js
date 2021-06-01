301f8da74f74a78fc3be4d3532740dfbf148d6ab
Hilary Richardson <hilary.richardson@gmail.com>
2021-06-01 00:11:54 -0700

    adding redux state util

---
+++ client/src/utils/State.js
@@ -0,0 +1,88 @@
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import moduleName from '../index';
+
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CURRENT_CATEGORY,
+    UPDATE_CATEGORIES,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART,
+} from "./actions.js";
+
+import { reducer } from "./reducers";
+
+const updateProducts = () => {
+    return {
+        type: UPDATE_PRODUCTS,
+    };
+};
+
+const updateCurrentCategory = () => {
+    return {
+        type: UPDATE_CURRENT_CATEGORY,
+    };
+};
+
+const updateCategories = () => {
+    return {
+        type: UPDATE_CATEGORIES,
+    };
+};
+
+const addToCart = () => {
+    return {
+        type: ADD_TO_CART,
+    };
+};
+
+const addMultipleToCart = () => {
+    return {
+        type: ADD_MULTIPLE_TO_CART,
+    };
+};
+
+const removeFromCart = () => {
+    return {
+        type: REMOVE_FROM_CART,
+    };
+};
+
+const updateCartQuantity = () => {
+    return {
+        type: UPDATE_CART_QUANTITY,
+    };
+};
+
+const clearCart = () => {
+    return {
+        type: CLEAR_CART,
+    };
+};
+
+const toggleCart = () => {
+    return {
+        type: TOGGLE_CART,
+    };
+};
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: "",
+        cartOpen: false,
+        cart: [],
+    });
+    //use this to confirm it works!
+    console.log(state);
+
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const store = createStore(reducer);
